5ce3cff7650dbd577b4bc9e36e4941ba213d0b04
kerenfranco2024 <144062924+kerenfranco2024@users.noreply.github.com>
2024-11-26 13:34:02 -0600

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -88,12 +88,12 @@ export function Navbar() {
                                     >
                                         Sign In
                                     </Link>
-                                    <a
+                                    <Link
                                         href="#"
                                         className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                     >
                                         Calendar
-                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
